2d780b94cbe522ab96c1d3e16fb0cdde1d989962
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-02-16 16:28:35 +0000

    changed node v

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v18
+nvm install v16
 
 npm install
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,7 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v18
+nvm install v16
 
 npm install pm2 -g
 pm2 startOrReload ecosystem.config.json
