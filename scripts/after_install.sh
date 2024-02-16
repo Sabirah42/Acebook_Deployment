95f73a287e3211f3dfb5c770c8036bb54efc0618
mohamed <mohamed.abdisalam@gmail.com>
2024-02-16 12:30:25 +0000

    Changed the order of installing node

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,5 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
 nvm use --delete-prefix v21.6.2
-npm install
+nvm install node
