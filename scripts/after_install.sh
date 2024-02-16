439cefddc529385c745c867cbacaa56557166bd8
mohamed <mohamed.abdisalam@gmail.com>
2024-02-16 12:25:45 +0000

    Added after install script again

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+cd /var/www/html/yusabmoli-ec2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+nvm use --delete-prefix v21.6.2
+npm install
