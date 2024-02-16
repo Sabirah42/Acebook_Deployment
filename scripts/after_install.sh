0379bd0641072f94df27f3785452ed68be0a2350
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-02-16 15:32:39 +0000

    reverted scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 cd /var/www/html/yusabmoli-ec2
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm use --delete-prefix v21.6.2
 nvm install node
 
 npm install
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,8 @@
 cd /var/www/html/yusabmoli-ec2
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm use --delete-prefix v21.6.2
 nvm install node
+npm start
 
-npm install pm2 -g
-pm2 startOrReload ecosystem.config.json
+
