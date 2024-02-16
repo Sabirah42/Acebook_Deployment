ab8ccddc86ec916b10f204725d4f77a8820988b5
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-02-16 16:24:34 +0000

    changed zip file to exclude node modules and hidden files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v14
+nvm install v18
 
 npm install
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,7 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v14
+nvm install v18
 
 npm install pm2 -g
 pm2 startOrReload ecosystem.config.json
