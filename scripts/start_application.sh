76c79038f407e917d061fc9e8b711423373ff08a
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-02-16 15:59:21 +0000

    reinstated ecostystem config file

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,12 @@
 cd /var/www/html/yusabmoli-ec2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm use --delete-prefix v21.6.2
-nvm install node
+nvm install v21.6.2
+
+npm install pm2 -g
+pm2 startOrReload ecosystem.config.json
+
+
+
 
 
