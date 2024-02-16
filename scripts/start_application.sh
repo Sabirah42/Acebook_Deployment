14cfcf44629d45aa99bf913e4284f5b7d6deec11
Sabirah <sabirah.miah.ahammed@gmail.com>
2024-02-16 14:58:10 +0000

    changed delete prefix

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,7 +2,7 @@ cd /var/www/html/yusabmoli-ec2
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm use --delete-prefix v21.6.2
+nvm use --delete-prefix v21.6.2 --silent
 nvm install node
 
 npm install pm2 -g
